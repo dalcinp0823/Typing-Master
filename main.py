64f4c96bda89088af9d8d87c8abd12d3d30fc0c2
dalcinp0823 <dalcinp0823@gmail.com>
2026-02-18 13:40:04 -0400

    Added good luck message

---
+++ main.py
@@ -0,0 +1,34 @@
+import random
+print("Welcome to Typing Master")
+print("------------------------")
+words = [
+    "El Alfa", "Rochy RD", "Juan Soto", "Jeudry", "Computadora",
+    "Flauta", "Arroz", "Bandera Dominicana", "Celular", "Facil",
+    "Salsa", "Baul", "Ferrari", "Bachata", "Dal Cin"
+]
+
+random.shuffle(words)
+
+mistakes = 0
+correct = 0
+total_words = len(words)
+
+for word in words:
+    print("Type this word:", word)
+    user = input()
+
+    if user == word:
+        print("Correct!")
+        correct += 1
+    else:
+        print("Incorrect!")
+        mistakes += 1
+
+    print("Progress:", correct + mistakes, "/", total_words)
+    print("----------------------")
+
+print("Game Over")
+print("Mistakes:", mistakes)
+
+accuracy = (correct / total_words) * 100
+print("Accuracy:", round(accuracy, 2), "%")
